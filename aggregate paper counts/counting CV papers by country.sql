9df97bd3ed355e85d4847e7a105990eda4198901
aa2291 <aa2291@georgetown.edu>
2021-07-09 01:18:13 -0400

    attempted change to CSET standard SQL
    using James's XML standard and PyCharm's "Reformat" function

--- aggregate paper counts/counting CV papers by country.sql
+++ aggregate paper counts/counting CV papers by country.sql
@@ -1,82 +1,84 @@
 --filter for recent CV papers with a country
-WITH recent_cv_papers AS(
-SELECT distinct cset_id as merged_id, meta.year as year 
+WITH recent_cv_papers AS (
+    SELECT distinct cset_id as merged_id, meta.year as year
 from article_classification.predictions preds
-INNER JOIN gcp_cset_links_v2.paper_affiliations_merged affs ON(preds.cset_id = affs.merged_id)
-INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING(merged_id)
+    INNER JOIN gcp_cset_links_v2.paper_affiliations_merged affs
+ON(preds.cset_id = affs.merged_id)
+    INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING (merged_id)
 WHERE cv = TRUE
-AND country IS NOT NULL
-AND meta.year >= 2015 AND meta.year <=2019
-),
+  AND country IS NOT NULL
+  AND meta.year >= 2015
+  AND meta.year <=2019
+    ),
 
 --filter for recent CV papers with a country and task
-recent_cv_task_papers AS(
+    recent_cv_task_papers AS (
 SELECT merged_id, year
 FROM recent_cv_papers
-INNER JOIN tasks_and_methods.tasks USING(merged_id)
+    INNER JOIN tasks_and_methods.tasks USING (merged_id)
 WHERE spans IS NOT NULL
-),
+    ),
 
 --count the number of countries for each CV paper
-paper_country_counts AS(
-SELECT merged_id, COUNT(DISTINCT country) as n_countries
+    paper_country_counts AS (
+SELECT merged_id, COUNT (DISTINCT country) as n_countries
 FROM recent_cv_task_papers
-INNER JOIN gcp_cset_links_v2.paper_affiliations_merged USING(merged_id)
+    INNER JOIN gcp_cset_links_v2.paper_affiliations_merged USING (merged_id)
 WHERE country IS NOT NULL
 GROUP BY merged_id
-),
+    ),
 
 --for each country, count the total share of CV papers associated with that country
 -- -- for a given task and year
 -- NP_norm = normalized count (dividing a paper across its contributing countries). This is the figure we use for our comparisons.
 -- NP = non-normalized count, multiple-counting a paper for each of its countries. 
-country_year_counts AS(
+    country_year_counts AS (
 SELECT country, year,
-COUNT(DISTINCT merged_id) as NP,
-SUM(1./n_countries) as NP_norm
+    COUNT (DISTINCT merged_id) as NP,
+    SUM (1./n_countries) as NP_norm
 FROM paper_country_counts
-INNER JOIN (SELECT DISTINCT merged_id, country FROM gcp_cset_links_v2.paper_affiliations_merged) USING(merged_id)
-INNER JOIN gcp_cset_links_v2.article_merged_meta USING(merged_id)
+    INNER JOIN (SELECT DISTINCT merged_id, country FROM gcp_cset_links_v2.paper_affiliations_merged) USING (merged_id)
+    INNER JOIN gcp_cset_links_v2.article_merged_meta USING (merged_id)
 WHERE country IS NOT NULL
 GROUP BY country, year
-),
+    ),
 
 
 --tag countries by membership in one of several groups
 --note: the groups do not overlap. Countries not included in a specific group are tagged with the "Other" group.
-country_groups AS (
-    SELECT DISTINCT
-      (country),
-      (CASE
-        WHEN country IN
-             ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus",
-              "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania",
-              "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland") 
-        THEN "EU"
-     WHEN country IN ("Norway", "Switzerland", "Liechtenstein", "Iceland")
-     THEN "EFTA"
-     WHEN country IN ("United Kingdom", "Canada", "Australia", "New Zealand")
-     THEN "CANZUK"
-     WHEN country IN ("Japan", "Korea", "Taiwan")
-     THEN "East Asian democracy" 
-     WHEN country IN
-             ("Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Philippines", "Myanmar", "Cambodia", "Laos",
-              "Brunei") 
-        THEN "SE Asia"
-     WHEN country IN
-             ("Egypt", "Saudi Arabia", "Turkey", "Iran", "Iraq", "Israel", "Yemen", "Oman", "Syria", "Jordan",
-              "Lebanon", "Kuwait", "Qatar", "United Arab Emirates", "Pakistan") 
-     THEN "Middle East"
-     WHEN country IN("China") THEN "China"
-     WHEN country IN("United States") THEN "United States"
-     WHEN country IN("India") THEN "India"
-     ELSE "Other" 
-     END) as country_group
-    FROM `gcp_cset_links_v2.affiliations_merged`
-  )
+    country_groups AS (
+SELECT DISTINCT
+    (country),
+    (CASE
+    WHEN country IN
+    ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus",
+    "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania",
+    "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland")
+    THEN "EU"
+    WHEN country IN ("Norway", "Switzerland", "Liechtenstein", "Iceland")
+    THEN "EFTA"
+    WHEN country IN ("United Kingdom", "Canada", "Australia", "New Zealand")
+    THEN "CANZUK"
+    WHEN country IN ("Japan", "Korea", "Taiwan")
+    THEN "East Asian democracy"
+    WHEN country IN
+    ("Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Philippines", "Myanmar", "Cambodia", "Laos",
+    "Brunei")
+    THEN "SE Asia"
+    WHEN country IN
+    ("Egypt", "Saudi Arabia", "Turkey", "Iran", "Iraq", "Israel", "Yemen", "Oman", "Syria", "Jordan",
+    "Lebanon", "Kuwait", "Qatar", "United Arab Emirates", "Pakistan")
+    THEN "Middle East"
+    WHEN country IN ("China") THEN "China"
+    WHEN country IN ("United States") THEN "United States"
+    WHEN country IN ("India") THEN "India"
+    ELSE "Other"
+    END) as country_group
+FROM `gcp_cset_links_v2.affiliations_merged`
+    )
 
 --present paper counts by country, year, and task. We tag each country with its group so that we can aggregate country-group level stats in the resulting Google Sheet. 
-SELECT year, country, "CV" as category, NP, NP_norm, country_group  
+SELECT year, country, "CV" as category, NP, NP_norm, country_group
 FROM country_year_counts
-LEFT JOIN country_groups USING(country)
+    LEFT JOIN country_groups USING (country)
 ORDER BY year DESC, country_group ASC, country ASC
--- identifying surveillance-task papers/finding top CV tasks/counting 2019 CV papers in common task categories.sql
+++ identifying surveillance-task papers/finding top CV tasks/counting 2019 CV papers in common task categories.sql
@@ -2,44 +2,46 @@
 --In particular, we can see that visual surveillance of humans is a fairly common task area; aside from very common domain-general tasks (e.g. "classification"), it is the most common.
 
 --select computer vision papers from 2019
-WITH cv_papers AS(
-SELECT distinct cset_id as merged_id, meta.year as year 
+WITH cv_papers AS (
+    SELECT distinct cset_id as merged_id, meta.year as year
 from article_classification.predictions preds
-INNER JOIN gcp_cset_links_v2.paper_affiliations_merged affs ON(preds.cset_id = affs.merged_id)
-INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING(merged_id)
+    INNER JOIN gcp_cset_links_v2.paper_affiliations_merged affs
+ON(preds.cset_id = affs.merged_id)
+    INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING (merged_id)
 WHERE cv = TRUE
-AND country IS NOT NULL
-AND meta.year = 2019
-),
+  AND country IS NOT NULL
+  AND meta.year = 2019
+    ),
 
 --tag papers with a common task category ("general", "surveillance", "medical", etc.) if it contains at least one relevant term
 -- -- terms are taken from the top 100 terms, so we are missing some alternative phrasings, e.g. "fer" for facial expression recognition
-CV_paper_tasks AS(
+    CV_paper_tasks AS (
 SELECT merged_id,
-MAX(CASE WHEN span in ('classification', 'segmentation', 'detection', 'computer vision', 'image processing', 'object detection', 'recognition', 'feature extraction', 'image segmentation', 'image classification', 'semantic segmentation', 'tracking', 'identification', 'artificial intelligence', 'object recognition', 'prediction', 'registration', 'machine learning', 'data augmentation', 'reconstruction', 'image recognition', 'training', 'retrieval', 'image analysis', 'image retrieval', 'pattern recognition', 'sr', 'learning', 'optimization', 'localization', 'generalization', 'computer vision tasks', 'denoising', 'analysis', 'clustering', 'inference', 'matching', 'image denoising', 'machine vision', 'object tracking', 'training process', 'edge detection', 'image enhancement', 'image registration', 'visual tracking', 'change detection', '3d reconstruction', 'deep learning', 'diabetic retinopathy', 'preprocessing', 'computer vision applications', 'image fusion', 'real-world applications', 'classification task', 'image reconstruction', 'robotics', 'image acquisition', 'processing', 'image restoration', 'optimization problem', 'ai', 'classification tasks', 'saliency detection', 'image preprocessing', 'manual segmentation', 'depth estimation', 'validation', 'dr', 'dimensionality reduction') THEN 1 ELSE 0 END) as general,
-MAX(CASE WHEN span in ('face recognition', 'person re-identification', 'action recognition', 'face detection', 'facial expression recognition', 'video surveillance', 'human action recognition', 're-id', 'security', 'surveillance') THEN 1 ELSE 0 END) as surveillance,
-MAX(CASE WHEN span in ('diagnosis', 'breast cancer', 'medical image analysis', 'medical image segmentation', 'computer-aided diagnosis', 'treatment') THEN 1 ELSE 0 END) as medical,
-MAX(CASE WHEN span in ('agriculture') THEN 1 ELSE 0 END) as crop_analysis,
-MAX(CASE WHEN span in ('autonomous driving', 'autonomous vehicles') THEN 1 ELSE 0 END) as autonomous_vehicles,
-MAX(CASE WHEN span in ('remote sensing', 'hyperspectral image classification', 'fusion', 'super-resolution', 'sar', 'pedestrian detection', 'target detection', 'remote sensing images', 'image super-resolution') THEN 1 ELSE 0 END) as other
+    MAX (CASE WHEN span in ('classification', 'segmentation', 'detection', 'computer vision', 'image processing', 'object detection', 'recognition', 'feature extraction', 'image segmentation', 'image classification', 'semantic segmentation', 'tracking', 'identification', 'artificial intelligence', 'object recognition', 'prediction', 'registration', 'machine learning', 'data augmentation', 'reconstruction', 'image recognition', 'training', 'retrieval', 'image analysis', 'image retrieval', 'pattern recognition', 'sr', 'learning', 'optimization', 'localization', 'generalization', 'computer vision tasks', 'denoising', 'analysis', 'clustering', 'inference', 'matching', 'image denoising', 'machine vision', 'object tracking', 'training process', 'edge detection', 'image enhancement', 'image registration', 'visual tracking', 'change detection', '3d reconstruction', 'deep learning', 'diabetic retinopathy', 'preprocessing', 'computer vision applications', 'image fusion', 'real-world applications', 'classification task', 'image reconstruction', 'robotics', 'image acquisition', 'processing', 'image restoration', 'optimization problem', 'ai', 'classification tasks', 'saliency detection', 'image preprocessing', 'manual segmentation', 'depth estimation', 'validation', 'dr', 'dimensionality reduction') THEN 1 ELSE 0 END) as general,
+    MAX (CASE WHEN span in ('face recognition', 'person re-identification', 'action recognition', 'face detection', 'facial expression recognition', 'video surveillance', 'human action recognition', 're-id', 'security', 'surveillance') THEN 1 ELSE 0 END) as surveillance,
+    MAX (CASE WHEN span in ('diagnosis', 'breast cancer', 'medical image analysis', 'medical image segmentation', 'computer-aided diagnosis', 'treatment') THEN 1 ELSE 0 END) as medical,
+    MAX (CASE WHEN span in ('agriculture') THEN 1 ELSE 0 END) as crop_analysis,
+    MAX (CASE WHEN span in ('autonomous driving', 'autonomous vehicles') THEN 1 ELSE 0 END) as autonomous_vehicles,
+    MAX (CASE WHEN span in ('remote sensing', 'hyperspectral image classification', 'fusion', 'super-resolution', 'sar', 'pedestrian detection', 'target detection', 'remote sensing images', 'image super-resolution') THEN 1 ELSE 0 END) as other
 
 FROM tasks_and_methods.tasks task_data
-CROSS JOIN UNNEST(spans) as span
-INNER JOIN cv_papers USING(merged_id)
+    CROSS JOIN UNNEST(spans) as span
+    INNER JOIN cv_papers USING (merged_id)
 
 GROUP BY merged_id
-),
+    ),
 
 --for each category, count the 2019 CV papers in which a relevant term appears 
-category_counts AS(
+    category_counts AS (
 SELECT
-SUM(general) as general,
-SUM(surveillance) as surveillance,
-SUM(medical) as medical,
-SUM(crop_analysis) as crop_analysis,
-SUM(autonomous_vehicles) as autonomous_vehicles,
-SUM(other) as other
+    SUM (general) as general,
+    SUM (surveillance) as surveillance,
+    SUM (medical) as medical,
+    SUM (crop_analysis) as crop_analysis,
+    SUM (autonomous_vehicles) as autonomous_vehicles,
+    SUM (other) as other
 FROM CV_paper_tasks)
 
 
-SELECT * from category_counts
+SELECT *
+from category_counts
