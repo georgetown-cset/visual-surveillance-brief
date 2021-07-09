c45dc0db04c38fa735875fc53d9efaf49104a1a9
aa2291 <73199748+aa2291@users.noreply.github.com>
2021-07-09 01:19:11 -0400

    attempted change to CSET standard SQL (#19)
    using James's XML standard and PyCharm's "Reformat" function

--- aggregate paper counts/counting all surv papers by country.sql
+++ aggregate paper counts/counting all surv papers by country.sql
@@ -1,84 +1,73 @@
-WITH task_family_pairs AS(
-SELECT DISTINCT parent_task as task_family, raw_task 
-from surveillance_tasks_brief.task_parent_pairs),
-
-recent_cv_papers AS(
-SELECT distinct cset_id as merged_id, meta.year as year 
-from article_classification.predictions preds
-INNER JOIN gcp_cset_links_v2.paper_affiliations_merged affs ON(preds.cset_id = affs.merged_id)
-INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING(merged_id)
-WHERE cv_filtered = TRUE
-AND country IS NOT NULL
-AND meta.year >= 2015 AND meta.year <=2019
+WITH task_family_pairs AS (
+    SELECT DISTINCT parent_task AS task_family,
+                    raw_task
+    FROM surveillance_tasks_brief.task_parent_pairs
 ),
+
+     recent_cv_papers AS (
+         SELECT DISTINCT cset_id AS merged_id,
+                         meta.year AS YEAR
+FROM article_classification.predictions preds
+INNER JOIN gcp_cset_links_v2.paper_affiliations_merged affs ON(preds.cset_id = affs.merged_id)
+INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING (merged_id)
+
+WHERE
+    cv_filtered = TRUE
+  AND country IS NOT NULL
+  AND meta.year >= 2015
+  AND meta.year <=2019 ),
 
 
 --get recent computer vision papers that contain surveillance tasks
-task_papers AS(
-SELECT merged_id, raw_task, task_family as base_task, year
-FROM tasks_and_methods.tasks task_data
-CROSS JOIN UNNEST(spans) as span
-INNER JOIN recent_cv_papers USING(merged_id)
-INNER JOIN task_family_pairs ON(span = task_family_pairs.raw_task)
-),
+    task_papers AS (
+SELECT
+    merged_id, raw_task, task_family AS base_task, YEAR
+FROM tasks_and_methods.tasks task_data CROSS JOIN UNNEST(spans) AS span INNER JOIN recent_cv_papers USING (merged_id) INNER JOIN task_family_pairs
+ON(span = task_family_pairs.raw_task) ),
 
---get all papers with at least one surveillance task
-surv_papers AS(
-SELECT DISTINCT merged_id, year FROM task_papers),
-
-
-paper_country_counts AS(
-SELECT merged_id, COUNT(DISTINCT country) as n_countries
-FROM surv_papers
-INNER JOIN gcp_cset_links_v2.paper_affiliations_merged USING(merged_id)
-WHERE country IS NOT NULL
-GROUP BY merged_id
-),
-
-country_task_counts AS(
-SELECT country, year,
-COUNT(DISTINCT merged_id) as NP,
-SUM(1./n_countries) as NP_norm
-FROM paper_country_counts
-INNER JOIN surv_papers USING(merged_id)
-INNER JOIN (SELECT DISTINCT merged_id, country FROM gcp_cset_links_v2.paper_affiliations_merged) USING(merged_id)
-WHERE country IS NOT NULL
-GROUP BY country, year
-),
+    --get all papers with at least one surveillance task
+    surv_papers AS (
+SELECT
+    DISTINCT merged_id, YEAR
+FROM task_papers), paper_country_counts AS (
+SELECT
+    merged_id, COUNT (
+    DISTINCT country) AS n_countries
+FROM surv_papers INNER JOIN gcp_cset_links_v2.paper_affiliations_merged USING (merged_id)
+WHERE
+    country IS NOT NULL
+GROUP BY
+    merged_id ), country_task_counts AS (
+SELECT
+    country, YEAR, COUNT (
+    DISTINCT merged_id) AS NP, SUM (
+    1./n_countries) AS NP_norm
+FROM paper_country_counts INNER JOIN surv_papers USING (merged_id) INNER JOIN (SELECT DISTINCT merged_id, country FROM gcp_cset_links_v2.paper_affiliations_merged) USING (merged_id)
+WHERE
+    country IS NOT NULL
+GROUP BY
+    country, YEAR ),
 
 
---tag countries by membership in one of several groups
---note: the groups do not overlap. they also don't cover the full set of countries.
-  country_groups AS (
-    SELECT DISTINCT
-      (country),
-      (CASE
-        WHEN country IN
-             ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus",
-              "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania",
-              "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland") 
-        THEN "EU"
-     WHEN country IN ("United Kingdom", "Canada", "Australia", "New Zealand")
-     THEN "CANZUK"
-     WHEN country IN ("Japan", "Korea", "Taiwan")
-     THEN "East Asian democracy" 
-     WHEN country IN
-             ("Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Philippines", "Myanmar", "Cambodia", "Laos",
-              "Brunei") 
-        THEN "SE Asia"
-     WHEN country IN
-             ("Egypt", "Saudi Arabia", "Turkey", "Iran", "Iraq", "Israel", "Yemen", "Oman", "Syria", "Jordan",
-              "Lebanon", "Kuwait", "Qatar", "United Arab Emirates", "Pakistan") 
-     THEN "Middle East"
-     WHEN country IN("China") THEN "China"
-     WHEN country IN("United States") THEN "United States"
-     WHEN country IN("India") THEN "India"
-     ELSE "Other" 
-     END) as country_group
-    FROM `gcp_cset_links_v2.affiliations_merged`
-  )
+    --tag countries by membership in one of several groups
+    --note: the groups do not overlap. they also don't cover the full set of countries.
+    country_groups AS (
+SELECT
+    DISTINCT (
+    country), (
+    CASE WHEN country IN (
+    "Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus", "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania", "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland") THEN "EU" WHEN country IN (
+    "United Kingdom", "Canada", "Australia", "New Zealand") THEN "CANZUK" WHEN country IN (
+    "Japan", "Korea", "Taiwan") THEN "East Asian democracy" WHEN country IN (
+    "Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Philippines", "Myanmar", "Cambodia", "Laos", "Brunei") THEN "SE Asia" WHEN country IN (
+    "Egypt", "Saudi Arabia", "Turkey", "Iran", "Iraq", "Israel", "Yemen", "Oman", "Syria", "Jordan", "Lebanon", "Kuwait", "Qatar", "United Arab Emirates", "Pakistan") THEN "Middle East" WHEN country IN (
+    "China") THEN "China" WHEN country IN (
+    "United States") THEN "United States" WHEN country IN (
+    "India") THEN "India" ELSE "Other" END) AS country_group
+FROM `gcp_cset_links_v2.affiliations_merged` )
 
-SELECT year, country, NP, NP_norm, country_group  
-FROM country_task_counts
-LEFT JOIN country_groups USING(country)
-ORDER BY year DESC, country_group ASC, country ASC
+SELECT
+    YEAR, country, NP, NP_norm, country_group
+FROM country_task_counts LEFT JOIN country_groups USING (country)
+ORDER BY
+    YEAR DESC, country_group ASC, country ASC
--- comparing paper growth across surv, CV, AI, all research/counting all research by country.sql
+++ comparing paper growth across surv, CV, AI, all research/counting all research by country.sql
@@ -1,63 +1,63 @@
-WITH recent_papers AS(
-SELECT distinct merged_id, meta.year as year 
-from gcp_cset_links_v2.paper_affiliations_merged affs 
-INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING(merged_id)
+WITH recent_papers AS (
+    SELECT distinct merged_id, meta.year as year
+from gcp_cset_links_v2.paper_affiliations_merged affs
+    INNER JOIN gcp_cset_links_v2.article_merged_meta meta USING (merged_id)
 WHERE country IS NOT NULL
-AND meta.year >= 2015 AND meta.year <=2019
-),
+  AND meta.year >= 2015
+  AND meta.year <=2019
+    ),
 
-paper_country_counts AS(
-SELECT merged_id, COUNT(DISTINCT country) as n_countries
+  paper_country_counts AS (
+SELECT merged_id, COUNT (DISTINCT country) as n_countries
 FROM recent_papers
-INNER JOIN gcp_cset_links_v2.paper_affiliations_merged USING(merged_id)
+    INNER JOIN gcp_cset_links_v2.paper_affiliations_merged USING (merged_id)
 WHERE country IS NOT NULL
 GROUP BY merged_id
-),
-
-country_year_counts AS(
+    ),
+    country_year_counts AS (
 SELECT country, year,
-COUNT(DISTINCT merged_id) as NP,
-SUM(1./n_countries) as NP_norm
+    COUNT (DISTINCT merged_id) as NP,
+    SUM (1./n_countries) as NP_norm
 FROM paper_country_counts
-INNER JOIN (SELECT DISTINCT merged_id, country FROM gcp_cset_links_v2.paper_affiliations_merged) USING(merged_id)
-INNER JOIN gcp_cset_links_v2.article_merged_meta USING(merged_id)
+    INNER JOIN (SELECT DISTINCT merged_id, country FROM gcp_cset_links_v2.paper_affiliations_merged) USING (merged_id)
+    INNER JOIN gcp_cset_links_v2.article_merged_meta USING (merged_id)
 WHERE country IS NOT NULL
 GROUP BY country, year
-),
+    ),
 
 
 --tag countries by membership in one of several groups
 --note: the groups do not overlap. they also don't cover the full set of countries.
-  country_groups AS (
-    SELECT DISTINCT
-      (country),
-      (CASE
-        WHEN country IN
-             ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus",
-              "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania",
-              "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland") 
-        THEN "EU"
-     WHEN country IN ("United Kingdom", "Canada", "Australia", "New Zealand")
-     THEN "CANZUK"
-     WHEN country IN ("Japan", "Korea", "Taiwan")
-     THEN "East Asian democracy" 
-     WHEN country IN
-             ("Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Philippines", "Myanmar", "Cambodia", "Laos",
-              "Brunei") 
-        THEN "SE Asia"
-     WHEN country IN
-             ("Egypt", "Saudi Arabia", "Turkey", "Iran", "Iraq", "Israel", "Yemen", "Oman", "Syria", "Jordan",
-              "Lebanon", "Kuwait", "Qatar", "United Arab Emirates", "Pakistan") 
-     THEN "Middle East"
-     WHEN country IN("China") THEN "China"
-     WHEN country IN("United States") THEN "United States"
-     WHEN country IN("India") THEN "India"
-     ELSE "Other" 
-     END) as country_group
-    FROM `gcp_cset_links_v2.affiliations_merged`
-  )
+    country_groups AS (
+SELECT DISTINCT
+    (country),
+    (CASE
+    WHEN country IN
+    ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus",
+    "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania",
+    "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland")
+    THEN "EU"
+    WHEN country IN ("United Kingdom", "Canada", "Australia", "New Zealand")
+    THEN "CANZUK"
+    WHEN country IN ("Japan", "Korea", "Taiwan")
+    THEN "East Asian democracy"
+    WHEN country IN
+    ("Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Philippines", "Myanmar", "Cambodia", "Laos",
+    "Brunei")
+    THEN "SE Asia"
+    WHEN country IN
+    ("Egypt", "Saudi Arabia", "Turkey", "Iran", "Iraq", "Israel", "Yemen", "Oman", "Syria", "Jordan",
+    "Lebanon", "Kuwait", "Qatar", "United Arab Emirates", "Pakistan")
+    THEN "Middle East"
+    WHEN country IN ("China") THEN "China"
+    WHEN country IN ("United States") THEN "United States"
+    WHEN country IN ("India") THEN "India"
+    ELSE "Other"
+    END) as country_group
+FROM `gcp_cset_links_v2.affiliations_merged`
+    )
 
-SELECT year, country, "all" as category, NP, NP_norm, country_group  
+SELECT year, country, "all" as category, NP, NP_norm, country_group
 FROM country_year_counts
-LEFT JOIN country_groups USING(country)
+    LEFT JOIN country_groups USING (country)
 ORDER BY year DESC, country_group ASC, country ASC
