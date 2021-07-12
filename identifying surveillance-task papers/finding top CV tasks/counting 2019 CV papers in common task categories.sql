6e30a011d6e70d86bf86359bb611d42ed5c5b96f
aa2291 <73199748+aa2291@users.noreply.github.com>
2021-07-12 11:05:07 -0400

    Update counting 2019 CV papers in common task categories.sql

--- identifying surveillance-task papers/finding top CV tasks/counting 2019 CV papers in common task categories.sql
+++ identifying surveillance-task papers/finding top CV tasks/counting 2019 CV papers in common task categories.sql
@@ -17,12 +17,12 @@ WHERE cv_filtered = TRUE
 -- -- terms are taken from the top 100 terms, so we are missing some alternative phrasings, e.g. "fer" for facial expression recognition
     CV_paper_tasks AS (
 SELECT merged_id,
-    MAX (CASE WHEN span in ('classification', 'segmentation', 'detection', 'computer vision', 'image processing', 'object detection', 'recognition', 'feature extraction', 'image segmentation', 'image classification', 'semantic segmentation', 'tracking', 'identification', 'artificial intelligence', 'object recognition', 'prediction', 'registration', 'machine learning', 'data augmentation', 'reconstruction', 'image recognition', 'training', 'retrieval', 'image analysis', 'image retrieval', 'pattern recognition', 'sr', 'learning', 'optimization', 'localization', 'generalization', 'computer vision tasks', 'denoising', 'analysis', 'clustering', 'inference', 'matching', 'image denoising', 'machine vision', 'object tracking', 'training process', 'edge detection', 'image enhancement', 'image registration', 'visual tracking', 'change detection', '3d reconstruction', 'deep learning', 'diabetic retinopathy', 'preprocessing', 'computer vision applications', 'image fusion', 'real-world applications', 'classification task', 'image reconstruction', 'robotics', 'image acquisition', 'processing', 'image restoration', 'optimization problem', 'ai', 'classification tasks', 'saliency detection', 'image preprocessing', 'manual segmentation', 'depth estimation', 'validation', 'dr', 'dimensionality reduction') THEN 1 ELSE 0 END) as general,
-    MAX (CASE WHEN span in ('face recognition', 'person re-identification', 'action recognition', 'face detection', 'facial expression recognition', 'video surveillance', 'human action recognition', 're-id', 'security', 'surveillance') THEN 1 ELSE 0 END) as surveillance,
-    MAX (CASE WHEN span in ('diagnosis', 'breast cancer', 'medical image analysis', 'medical image segmentation', 'computer-aided diagnosis', 'treatment') THEN 1 ELSE 0 END) as medical,
-    MAX (CASE WHEN span in ('agriculture') THEN 1 ELSE 0 END) as crop_analysis,
+    MAX (CASE WHEN span in ('classification', 'segmentation', 'detection', 'computer vision', 'image processing', 'object detection', 'recognition', 'feature extraction', 'image segmentation', 'image classification', 'semantic segmentation', 'tracking', 'identification', 'artificial intelligence', 'object recognition', 'prediction', 'machine learning', 'image recognition', 'registration', 'data augmentation', 'reconstruction', 'image analysis', 'training', 'pattern recognition', 'retrieval', 'image retrieval', 'learning', 'optimization', 'localization', 'analysis', 'denoising', 'generalization', 'computer vision tasks', 'clustering', 'machine vision', 'matching', 'inference', 'image denoising', 'object tracking', 'edge detection', 'image enhancement', 'training process', 'image registration', 'change detection', 'visual tracking', 'preprocessing', 'deep learning', '3d reconstruction', 'image fusion', 'computer vision applications', 'image acquisition', 'robotics', 'real-world applications', 'image reconstruction', 'classification task', 'processing', 'image restoration', 'optimization problem', 'ai', 'image captioning', 'classification tasks', 'image preprocessing', 'saliency detection', 'pre-processing', 'manual segmentation', 'validation', 'dr') THEN 1 ELSE 0 END) as general,
+    MAX (CASE WHEN span in ('face recognition', 'action recognition', 'face detection', 'facial expression recognition', 'video surveillance', 'security', 'human action recognition', 're-id', 'surveillance') THEN 1 ELSE 0 END) as surveillance,
+    MAX (CASE WHEN span in ('diagnosis', 'medical imaging', 'breast cancer', 'medical image analysis', 'diabetic retinopathy', 'fusion', 'medical image segmentation', 'computer-aided diagnosis', 'treatment') THEN 1 ELSE 0 END) as medical,
+    MAX (CASE WHEN span in ('remote sensing', 'sar', 'target detection', 'remote sensing images') THEN 1 ELSE 0 END) as remote_sensing,
     MAX (CASE WHEN span in ('autonomous driving', 'autonomous vehicles') THEN 1 ELSE 0 END) as autonomous_vehicles,
-    MAX (CASE WHEN span in ('remote sensing', 'hyperspectral image classification', 'fusion', 'super-resolution', 'sar', 'pedestrian detection', 'target detection', 'remote sensing images', 'image super-resolution') THEN 1 ELSE 0 END) as other
+    MAX (CASE WHEN span in ('sr', 'pose estimation', 'hyperspectral image classification', 'super-resolution', 'pedestrian detection', 'agriculture', 'image super-resolution', 'depth estimation') THEN 1 ELSE 0 END) as other
 
 FROM tasks_and_methods.tasks task_data
     CROSS JOIN UNNEST(spans) as span
@@ -37,7 +37,7 @@ SELECT
     SUM (general) as general,
     SUM (surveillance) as surveillance,
     SUM (medical) as medical,
-    SUM (crop_analysis) as crop_analysis,
+    SUM (remote_sensing) as remote_sensing,
     SUM (autonomous_vehicles) as autonomous_vehicles,
     SUM (other) as other
 FROM CV_paper_tasks)
